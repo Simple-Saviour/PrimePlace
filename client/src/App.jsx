f3b1d89c6c5b5c69666795d2d2c6316339abe7ab
Simple Saviour <aryanchoudhary047@gmail.com>
2024-09-26 01:03:46 +0530

    completed SignUp functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/about';
 import Header from './components/Header';
@@ -12,8 +12,8 @@ export default function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/sign-in' element={<Signin />} />
-        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='/about' element={<About />} />
       </Routes>
