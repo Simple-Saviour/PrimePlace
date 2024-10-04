41ced93d059692b2c0641ef54b9d6c796a69e09c
Simple Saviour <aryanchoudhary047@gmail.com>
2024-10-04 23:27:07 +0530

    updated Profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/about';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateROute';
 
 export default function App() {
   return (
@@ -15,7 +16,9 @@ export default function App() {
         <Route path='/home' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
+        <Route element = {<PrivateRoute/>}>
         <Route path='/profile' element={<Profile />} />
+        </Route>
         <Route path='/about' element={<About />} />
       </Routes>
     </BrowserRouter>
